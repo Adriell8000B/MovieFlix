fe148c1009b91d1d6c1da65e6982355301190e35
Adriel <112901345+Adriell8000B@users.noreply.github.com>
2023-09-12 18:07:51 -0300

    Change api endpoint

--- src/lib/Movies.svelte
+++ src/lib/Movies.svelte
@@ -13,7 +13,7 @@
 	}
 
 	onMount(async () => {
-		const response = await fetch("https://movieflix-6dbj.onrender.com/")
+		const response = await fetch("https://movieflix-i23t.onrender.com/")
 		const data = await response.json()
 		movies = data
 	})
