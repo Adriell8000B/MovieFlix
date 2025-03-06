c6080905a4cb10f53639f4037a5fb4c5ea2d114d
Adriel <112901345+Adriell8000B@users.noreply.github.com>
2025-03-05 23:07:21 -0300

    Update Movies.svelte

--- src/lib/Movies.svelte
+++ src/lib/Movies.svelte
@@ -13,7 +13,7 @@
 	}
 
 	onMount(async () => {
-		const response = await fetch("https://movieflix-api-hp3y.onrender.com/movies")
+		const response = await fetch("https://movieflix-api-hp3y.onrender.com:8080/movies")
 		const data = await response.json()
 		movies = data
 	})
